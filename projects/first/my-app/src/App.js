0b35d6162f643c8a5850991abd7c257389b6caa2
Vyachelav Mryasov <slavamryasov@mail.ru>
2023-07-06 17:03:38 +0300

    third commit

--- projects/first/my-app/src/App.js
+++ projects/first/my-app/src/App.js
@@ -1,16 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
-      <img src={logo} />
+    <div className="modules">
+      <div className="app-wrapper">
+        <header className="header">
+          <img id="logo" src={logo} />
+        </header>
+        <nav className="nav">
+          <div><a>Profile</a></div>
+          <div><a>Messages</a></div>
+          <div><a>News</a></div>
+          <div><a>Music</a></div>
+          <div><a>Settings</a></div>
+        </nav>
+        <div className="content">
+          <div className="img">
+            <img className="background" src="https://fullhdoboi.ru/wp-content/uploads/_ph/6/283114741.jpg" />
+          </div>
+          <div className="data">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- projects/first/my-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return(
-    <div>
-      efsd
-    </div>
-  )
-};
-
-export default Footer;
