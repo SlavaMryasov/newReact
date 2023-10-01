60c7b211ff2a7fb22ef2f470dcd0e4d9628da155
Mryasov Vyacheslav <slavamryasov@mail.ru>
2023-10-01 21:01:50 +0300

    les 71 еще раз

--- projects/first/my-app/src/components/Profile/Data/MyProfile/ProfileStatus/ProfileStatus.jsx
+++ projects/first/my-app/src/components/Profile/Data/MyProfile/ProfileStatus/ProfileStatus.jsx
@@ -3,31 +3,27 @@ import React from "react";
 
 class ProfileStatus extends React.Component {
   state = {
-    modify: false
+    modify: false,
+    status: this.props.status
   }
 
-  changeModifyStatusOnTrue() {
-    //console.log(this.state.modify)
+  changeModifyStatus = (status) => {
     this.setState({ // АСИНХРОНЩИНА
-      modify: true
+      modify: status
     })
-    // console.log(this.state.modify)
   }
+  
 
-  changeModifyStatusOnFalse() { // контекст потеряется, надо байндить
-    this.setState({
-      modify: false
-    })
-  }
   render() {
+    console.log(this.props, 'ProfileStatus')
     return (
       <div>
         {!this.state.modify
           ? <div>
-            <span onDoubleClick={this.changeModifyStatusOnTrue.bind(this)}>{this.props.status}</span>
+            <span onDoubleClick={this.changeModifyStatus.bind(this, true)}>{this.props.status}</span>
           </div>
           : <div>
-            <input autoFocus={true} onBlur={this.changeModifyStatusOnFalse.bind(this)} value={this.props.status}></input>
+            <input  autoFocus={true} onBlur={this.changeModifyStatus.bind(this, false)} value={this.state.status}></input>
           </div>
         }
       </div>
